905730a1c5729ae79af6139ed6cc9e9f36eb9640
KentSteinwall <kentsteinwall@gmail.com>
2018-08-20 09:52:01 +0530

    insurance_register

---
+++ app/src/main/java/com/example/michelle/driverbuddy/Insurance_registration.java
@@ -0,0 +1,13 @@
+package com.example.michelle.driverbuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Insurance_registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insurance_registration);
+    }
+}

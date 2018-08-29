7e8fcad7b0b300a51fa1f0a8d40ede9d0597e014
KentSteinwall <kentsteinwall@gmail.com>
2018-08-29 16:51:38 +0530

    insurance_edit_profile,insurance_profile,insurance_track_driver

--- app/src/main/java/com/example/michelle/driverbuddy/Insurance_registration.java
+++ app/src/main/java/com/example/michelle/driverbuddy/Insurance_registration.java
@@ -1,13 +1,39 @@
 package com.example.michelle.driverbuddy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Insurance_registration extends AppCompatActivity {
+
+    public Button but1;
+
+    public void init()
+    {      but1 = (Button)findViewById(R.id.button3);
+           but1.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   Intent b= new Intent(Insurance_registration.this,insurance_profile.class);
+                   startActivity(b);
+               }
+           });
+
+
+
+
+
+
+    }
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insurance_registration);
+        init();
     }
 }

b105a864ab10a60ad794e264d1bdb20dc498bf54
shaveend <30971173+shaveend@users.noreply.github.com>
2018-09-05 19:04:03 +0530

    set up payment

--- app/src/main/java/com/example/michelle/driverbuddy/DriverMenu.java
+++ app/src/main/java/com/example/michelle/driverbuddy/DriverMenu.java
@@ -1,21 +1,40 @@
 package com.example.michelle.driverbuddy;
 
+import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class DriverMenu extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
 
+    public Button payfine_bttn;
+
+    public void setPayfine_bttn()
+    {
+        payfine_bttn=(Button)findViewById(R.id.payfine_bttn);
+        payfine_bttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent nextActivity= new Intent(DriverMenu.this,Payment.class);
+                startActivity(nextActivity);
+            }
+        });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_menu);
+        setPayfine_bttn();
 
         drawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
         actionBarDrawerToggle= new ActionBarDrawerToggle(this,drawerLayout,R.string.drawer_open,R.string.drawer_close);

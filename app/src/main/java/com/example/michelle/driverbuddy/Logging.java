95e34f6ec409658ab7f258a547bcf48d7bc99b36
shaveend <30971173+shaveend@users.noreply.github.com>
2018-09-08 20:04:27 +0530

    Merged all 3 aspects

--- app/src/main/java/com/example/michelle/driverbuddy/Logging.java
+++ app/src/main/java/com/example/michelle/driverbuddy/Logging.java
@@ -12,7 +12,7 @@ public class Logging extends AppCompatActivity {
     public Button btn_logIn,btn_signin;
 
 
-    /*public void driverMenuLogIn()
+    public void driverMenuLogIn()
     {
         btn_logIn=(Button)findViewById(R.id.btn_logIn);
 
@@ -21,11 +21,15 @@ public class Logging extends AppCompatActivity {
             public void onClick(View view) {
                 EditText username = (EditText)findViewById(R.id.editText);
                 EditText password = (EditText)findViewById(R.id.editText2);
-                if(username.getText().toString().equals("abc") && password.getText().toString().equals("abc")) {
+                if(username.getText().toString().equals("insurance") && password.getText().toString().equals("abc")) {
                     Intent nextActivity = new Intent(Logging.this, insurance_profile.class);
                     startActivity(nextActivity);
                 }
-                else
+                else if (username.getText().toString().equals("police") && password.getText().toString().equals("abc")){
+                    Intent nextActivity= new Intent(Logging.this,PoliceOfficerMenu.class);
+                    startActivity(nextActivity);
+                }
+                else if (username.getText().toString().equals("driver") && password.getText().toString().equals("abc"))
                 {
                     Intent nextActivity1 = new Intent(Logging.this, DriverMenu.class);
                     startActivity(nextActivity1);
@@ -34,21 +38,11 @@ public class Logging extends AppCompatActivity {
 
             }
         });
-    }*/
-
-    public void PoliceOfficerMenuLogIn()
-    {
-        btn_logIn=(Button)findViewById(R.id.btn_logIn);
-        btn_logIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent nextActivity= new Intent(Logging.this,PoliceOfficerMenu.class);
-                startActivity(nextActivity);
-            }
-        });
     }
 
+
+    public void signup()
+    {
         btn_signin=(Button)findViewById(R.id.btn_signUp);
         btn_signin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,8 +64,9 @@ public class Logging extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logging);
-        //driverMenuLogIn();
-        PoliceOfficerMenuLogIn();
+        driverMenuLogIn();
+        signup();
+
 
     }
 
